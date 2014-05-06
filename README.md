2ac97223cff3455bda817cc830b36bf77256ed93
Brian Birkhauser <brian.birkhauser@thebuddygroup.com>
2014-05-06 10:32:20 -0700

    refactored the code to remove dependency on john resig's class script and expose more methods to be called by ActivityTracker.method.

--- README.md
+++ README.md
@@ -22,23 +22,21 @@ Javascript client side activity tracker
 ================
 if you use activitytracker.full.min.js, these dependencies are already included. Otherwise you will have to include them in your project.
 - requires Scott Hamper's [Cookies](https://github.com/ScottHamper/Cookies)
-- requires John Resig's [class script](http://ejohn.org/blog/simple-javascript-inheritance/)
 
 ##How to use
 ================
 
 The easiest way to use is to add data attributes to the elements you want to track clicks on.  Setting the ```data-track``` attribute to either "true" or the name of the category you want to track the activity in.  You can optionally include trackable arguments as a data attribute. If no arguments are specified, it will use the href of the a tag or if it is not an a tag it will use the id of the element.
+
 ```<a href="#" data-track="myCategory" data-track-args="['argument 1', 'argument 2']">Trackable Link</a> ```
 
 
-```ActivityTracker.track(cat, arguments);```
 
 Manually track activity.  Must specify a category and arguments to track.
-
-```ActivityTracker.getData(cat); ```
+```ActivityTracker.track(cat, arguments);```
 
 Get the data for a specific category. If no category is specified, it will return an array with all the categories and data you have tracked.
-
-```ActivityTracer.clearData(cat); ```
+```ActivityTracker.getData(cat); ```
 
 Clears the cookie data for a specificy category. If no category is specified, it will clear all the data.
+```ActivityTracer.clearData(cat); ```
--- src/activitytracker.js
+++ src/activitytracker.js
@@ -1,87 +1,8 @@
 (function(undefined) {
     "use strict";
-    var ActivityTracker = Class.extend({
+    var ActivityTracker = {
         _keys: [],
-        init: function() {
-
-            if (!Cookies.enabled) {
-                console.log('Cannot track  - Cookies are disabled');
-                return;
-            }
-            Cookies.defaults = {
-                path: '/',
-                secure: false
-            }
-            var items = document.querySelectorAll('[data-track]');
-            var _tracker = this;
-            for (var i = 0; i < items.length; i++) {
-                var item = items[i];
-                item.addEventListener("click", function(ev) {
-                    _tracker._trackClick(ev.target);
-                });
-            }
-
-
-        },
-        track: function(cat, args) {
-            if (!Cookies.enabled) {
-                console.log('Cannot track  - Cookies are disabled');
-                return;
-            }
-            if (this._keys.indexOf(cat) == -1) {
-                this._keys.push(cat);
-                Cookies.set('ActivityTrackerKeys', JSON.stringify(this._keys));
-            }
-
-            var data = Cookies.get(cat);
-            if (!data) {
-                data = [];
-            } else {
-                data = JSON.parse(data);
-            }
-            var d = new Date();
-            var jsonDate = d.toJSON();
-            data.push({
-                date: jsonDate,
-                arguments: args
-            });
-            Cookies.set('ActivityTracker_' + cat, JSON.stringify(data));
-        },
-        getData: function(cat) {
-            if (!Cookies.enabled) {
-                console.log('Cannot track  - Cookies are disabled');
-                return;
-            }
-            var data = {};
-            if (!cat) {
-                this._keys = JSON.parse(Cookies.get('ActivityTrackerKeys'));
-                for (var i = 0; i < this._keys.length; i++) {
-                    cat = this._keys[i];
-                    data[cat] = JSON.parse(Cookies.get('ActivityTracker_' + cat));
-                }
-            } else {
-                data = JSON.parse(Cookies.get('ActivityTracker_' + cat));
-            }
-            console.debug(data);
-            return data;
-        },
-        clearData: function(cat) {
-            if (!Cookies.enabled) {
-                console.log('Cannot track  - Cookies are disabled');
-                return;
-            }
-            if (!cat) {
-                for (var i = 0; i < this._keys.length; i++) {
-                    cat = this._keys[i];
-                    Cookies.expire('ActivityTracker_' + cat);
-                    this._keys.splice(i, 1);
-                }
-            } else {
-                Cookies.expire('ActivityTracker_' + cat);
-            }
-        },
         _trackClick: function(item) {
-
             var category = item.getAttribute('data-track');
             if (category != 'true') {
                 category = 'default';
@@ -92,8 +13,93 @@
                 else args = item.getAttribute('id');
             }
             this.track(category, args);
+        },
+        init: function() {
+
+            if (!Cookies.enabled) {
+                console.log('Cannot track  - Cookies are disabled');
+                return;
+            }
+            Cookies.defaults = {
+                path: '/',
+                secure: false
+            }
+
+            this._keys = Cookies.get('ActivityTrackerKeys');
+            if (!this._keys) {
+                this._keys = [];
+            }
+            var items = document.querySelectorAll('[data-track]');
+            var _tracker = this;
+            for (var i = 0; i < items.length; i++) {
+                var item = items[i];
+                item.addEventListener("click", function(ev) {
+                    _tracker._trackClick(ev.target);
+                });
+            }
         }
-    });
+    };
+    ActivityTracker.track = function(cat, args) {
+        if (!Cookies.enabled) {
+            console.log('Cannot track  - Cookies are disabled');
+            return;
+        }
+        if (this._keys.indexOf(cat) == -1) {
+            this._keys.push(cat);
+            Cookies.set('ActivityTrackerKeys', JSON.stringify(this._keys));
+        }
+
+        var data = Cookies.get('ActivityTracker_' + cat);
+
+        if (!data) {
+            data = [];
+        } else {
+            data = JSON.parse(data);
+        }
+        var d = new Date();
+        var jsonDate = d.toJSON();
+        data.push({
+            date: jsonDate,
+            arguments: args
+        });
+        Cookies.set('ActivityTracker_' + cat, JSON.stringify(data));
+    };
+
+    ActivityTracker.getData = function(cat) {
+        if (!Cookies.enabled) {
+            console.log('Cannot track  - Cookies are disabled');
+            return;
+        }
+        var data = {};
+        if (!cat) {
+            this._keys = JSON.parse(Cookies.get('ActivityTrackerKeys'));
+            for (var i = 0; i < this._keys.length; i++) {
+                cat = this._keys[i];
+                data[cat] = JSON.parse(Cookies.get('ActivityTracker_' + cat));
+            }
+        } else {
+            data = JSON.parse(Cookies.get('ActivityTracker_' + cat));
+        }
+        console.debug(data);
+        return data;
+    };
+
+    ActivityTracker.clearData = function(cat) {
+        if (!Cookies.enabled) {
+            console.log('Cannot track  - Cookies are disabled');
+            return;
+        }
+        if (!cat) {
+            for (var i = 0; i < this._keys.length; i++) {
+                cat = this._keys[i];
+                Cookies.expire('ActivityTracker_' + cat);
+                this._keys.splice(i, 1);
+            }
+        } else {
+            Cookies.expire('ActivityTracker_' + cat);
+        }
+    };
+
 
     // AMD support
     if (typeof define === 'function' && define.amd) {
@@ -111,4 +117,7 @@
     } else {
         window.ActivityTracker = ActivityTracker;
     }
+
+    ActivityTracker.init();
+
 })();
